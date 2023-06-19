ea5d750ec4bcb3c84b8c1b97c5745ec100427bc0
Gustavo Zimbron <pptavozl@gmail.com>
2023-06-19 12:04:16 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//import Compressor from './classes/compressor.js';
+
+//comprimir carpeta
+/* const resultado =  await Compressor.folderCompress(
+	'/Users/zimbronapps/Downloads/redimensionador/originales',
+	'/Users/zimbronapps/Downloads/redimensionador/optimizadas',
+	{
+		maxWidth: 1500,
+		maxHeight: 1500,
+		pngToWebp: true
+	}
+); */
+
+//comprimir una sola
+/* const result = await Compressor.imageCompress(
+	'/Users/zimbronapps/Downloads/redimensionador/originales/arbol.png',
+	'/Users/zimbronapps/Downloads/redimensionador/arbol222.png',
+	{
+		quality: 20,
+		maxWidth: 1500,
+		maxHeight: 1500,
+		pngToWebp: true
+	}
+); */
